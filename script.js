2a009abc0d25adcde45e8872a92324f0a1961e06
Iqra <iqrayasir2609@gmail.com>
2023-07-02 19:42:48 +0100

    Create grid

--- script.js
+++ script.js
@@ -1,13 +1,22 @@
 const container = document.querySelector(`.container`);
-const rowArray = [];
 for (let i = 0; i < 16; i++) {
     const row = document.createElement(`div`);
     row.classList.add(`${i}`);
     row.setAttribute(`style`, 
     `border: 1px solid black;
-    flex: auto;`);
-    rowArray.push(row)
-    container.appendChild(rowArray[i]);
+    flex: auto;
+    display: flex;`);
+    for (let k = 0; k < 16; k++) {
+        const square = document.createElement(`div`);
+        square.classList.add(`${(k + 1)*(i + 1) - 1}`);
+        square.setAttribute(`style`,
+        `border: 1px solid black;
+        flex: auto;`);
+        row.appendChild(square);
+        if ((i < 16) && (k == 16)) {
+            k = 0;
+            break;
+        } 
+    }
+    container.appendChild(row);
 }
-
-
